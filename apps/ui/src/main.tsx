a3b384870ddcd5d21008a5e1386a61f8b43bdbba
Tal Kovler <tal06030@googlemail.com>
2024-06-22 23:15:01 +0300

    added PWA support

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateServiceWorkers = registerSW({
+  onNeedRefresh() {
+    if (confirm('New Content Available. Reload?')) {
+      updateServiceWorkers(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
